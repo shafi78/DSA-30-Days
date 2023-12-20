4f43124571d284af27d256a8c9313b2ea8fc227b
shafi <shafiullaattar786@gmail.com>
2023-12-20 17:53:56 +0530

    OOps

--- Day 09/S_10_practise_q_1.java
+++ Day 09/S_10_practise_q_1.java
@@ -1,4 +1,8 @@
 // Find out the correct statement to assign name to objects
+// a. s1->name = "value"
+// b. Student.name = "value"
+// c. s1.name = "value"
+
 
 public class S_10_practise_q_1 {
     public static void main(String args[]) {
